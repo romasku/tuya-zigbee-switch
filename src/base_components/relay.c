462f881ea1b93e7de7bc7c9fe8c7f3e55fc53d59
aceindy <aceindy@gmail.com>
2025-07-10 14:20:55 +0200

    Update relay.c
    Latch relays require an on/off pulse instead of continuous power.
    When left on, the unit draws too much power since it doesn't have a Neutral wire. When left on, my 2 channel unit (2x400mW) gets stuck in a boot loop.
    
    According [datasheet](https://datasheet.octopart.com/HF3F-L-12-1HL1T%28257%29-Hongfa-datasheet-145013251.pdf) it should be minimum 100ms

--- src/base_components/relay.c
+++ src/base_components/relay.c
@@ -11,11 +11,17 @@ void relay_init(relay_t *relay)
 void relay_on(relay_t *relay)
 {
   printf("relay_on\r\n");
-  drv_gpio_write(relay->pin, relay->on_high);
-  if (relay->off_pin)
-  {
-    drv_gpio_write(relay->off_pin, !relay->on_high);
+  
+  if (relay->off_pin) {
+    // Bi-stable latch relay: Pulse ON pin for 125ms
+    drv_gpio_write(relay->pin, relay->on_high);
+    WaitMs(125);
+    drv_gpio_write(relay->pin, !relay->on_high);
+  } else {
+    // Normal relay: Turn on the active pin
+    drv_gpio_write(relay->pin, relay->on_high);
   }
+
   relay->on = 1;
   if (relay->on_change != NULL)
   {
@@ -26,11 +32,17 @@ void relay_on(relay_t *relay)
 void relay_off(relay_t *relay)
 {
   printf("relay_off\r\n");
-  drv_gpio_write(relay->pin, !relay->on_high);
-  if (relay->off_pin)
-  {
+
+  if (relay->off_pin) {
+    // Bi-stable latch relay: Pulse OFF pin for 125ms
     drv_gpio_write(relay->off_pin, relay->on_high);
+    WaitMs(125);
+    drv_gpio_write(relay->off_pin, !relay->on_high);
+  } else {
+    // Normal relay: Turn off the active pin
+    drv_gpio_write(relay->pin, !relay->on_high);
   }
+
   relay->on = 0;
   if (relay->on_change != NULL)
   {
