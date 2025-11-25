fbea36d80db807048d38734f0269faf1fa1c85e3
Roman Skurikhin <romasku135@gmail.com>
2025-11-25 22:16:16 +0200

    Add option to allow multiple pulses at once

--- src/device_config/config_parser.h
+++ src/device_config/config_parser.h
@@ -10,6 +10,8 @@ extern network_indicator_t network_indicator;
 
 extern hal_zigbee_endpoint endpoints[10];
 
+extern uint8_t allow_simultaneous_latching_pulses;
+
 void parse_config();
 void init_reporting();
 void handle_version_changes();
