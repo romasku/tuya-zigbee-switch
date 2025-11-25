b322d3b7c3051aae2de33ebd80e5f5403f327251
Roman Skurikhin <romasku135@gmail.com>
2025-11-25 09:45:55 +0200

    Ensure mutual exclusion between relays

--- tests/test_base_components.py
+++ tests/test_base_components.py
@@ -1,27 +1,6 @@
 from tests.conftest import Device, wait_for
 
 
-def test_relay_off_pin_mutual_exclusion():
-    from tests.client import StubProc
-
-    # Relay R B0 with off pin C1
-    # Off-pin immediately follows the relay pin (no extra 'R' prefix)
-    cfg = "X;Y;SA0u;RB0C1;"
-    p = StubProc(device_config=cfg).start()
-    try:
-        d = Device(p)
-        # Relay endpoint is 2 (1 switch)
-        ep = 2
-        d.call_zigbee_cmd(ep, 0x0006, 0x01)
-        wait_for(lambda: d.get_gpio("B0") is True)
-        assert d.get_gpio("C1") is False
-        d.call_zigbee_cmd(ep, 0x0006, 0x00)
-        wait_for(lambda: d.get_gpio("B0") is False)
-        assert d.get_gpio("C1") is True
-    finally:
-        p.stop()
-
-
 def test_button_debounce_stronger(device: Device, button_pin: str, relay_pin: str):
     # Extended debounce: chatter beyond DEBOUNCE should collapse to a single press
     device.freeze_time()
