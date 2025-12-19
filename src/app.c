1a48c0eaae291dc2e4ca42aaf842557279ac3f3a
Bruno Mendonca <bruno.m.mendonca@gmail.com>
2025-12-19 05:36:30 +0000

    Report relay state on boot so Z2M knows current state after reboot

--- src/app.c
+++ src/app.c
@@ -2,6 +2,12 @@
 #include "hal/zigbee.h"
 #include "hal/zigbee_ota.h"
 #include "zigbee/general_commands.h"
+#include "zigbee/relay_cluster.h"
+#include "zigbee/consts.h"
+
+// Forward declarations from config_parser.c
+extern zigbee_relay_cluster relay_clusters[];
+extern uint8_t relay_clusters_cnt;
 
 void app_init(void) {
   handle_version_changes();
@@ -22,6 +28,12 @@ void app_task() {
   if (!boot_announce_sent &&
       hal_zigbee_get_network_status() == HAL_ZIGBEE_NETWORK_JOINED) {
     hal_zigbee_send_announce();
+    // Report all relay states on boot so Z2M knows the current state
+    for (uint8_t i = 0; i < relay_clusters_cnt; i++) {
+      hal_zigbee_notify_attribute_changed(relay_clusters[i].endpoint,
+                                          ZCL_CLUSTER_ON_OFF,
+                                          ZCL_ATTR_ONOFF);
+    }
     boot_announce_sent = true;
   }
 }
