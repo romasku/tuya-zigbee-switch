e2088428be5187fa51f87e48c89fad0ae21ddd25
Loránd Biró <lorand.biro@outlook.com>
2026-02-15 14:31:06 +0000

    Cover switch cluster

--- src/telink/hal/zigbee_zcl.c
+++ src/telink/hal/zigbee_zcl.c
@@ -4,6 +4,7 @@
 #include "zcl_include.h"
 #include "zcl_multistate_input.h"
 #include "zcl_onoff_configuration.h"
+#include "zcl_cover_switch_config.h"
 #pragma pack(pop)
 
 #include "telink_size_t_hack.h"
@@ -49,6 +50,9 @@ static cluster_registerFunc_t get_register_func_by_cluster_id(u16 cluster_id) {
     }
     if (cluster_id == ZCL_CLUSTER_CLOSURES_WINDOW_COVERING) { // Window Covering
         return zcl_windowCovering_register;
+    }
+    if (cluster_id == 0xFC01) { // Cover Switch Config
+        return zcl_cover_switch_config_register;
     }
     return NULL;
 }
