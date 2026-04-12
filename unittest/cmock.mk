a5dc1dab1cff39fa3b9d2537c7fc2af2f1f3d3c9
Hayden <haydendunnicliffe@gmail.com>
2026-04-12 16:11:12 +0100

    Setup make file so the CMake does not have to be included in the repo

---
+++ unittest/cmock.mk
@@ -0,0 +1,23 @@
+
+
+../test_tools:
+	mkdir -p ../test_tools
+
+../test_tools/cmock: | ../test_tools
+	mkdir -p ../test_tools/cmock
+
+../test_tools/cmock/download.zip: | ../test_tools/cmock
+	@echo "Downloading CMock"
+	curl -L "https://github.com/ThrowTheSwitch/CMock/archive/refs/tags/v2.6.0.zip" \
+	  -o ../test_tools/cmock/download.zip \
+		--fail --show-error --connect-timeout 30
+
+../test_tools/cmock/temp: | ../test_tools/cmock/download.zip
+	@echo "Extracting CMock"
+	@unzip -q ../test_tools/cmock/download.zip -d ../test_tools/cmock/temp
+
+../test_tools/cmock/src: | ../test_tools/cmock/temp
+	@echo "Copying Src CMock Files"
+	mv ../test_tools/cmock/temp/*/* ../test_tools/cmock
+
+setup: | ../test_tools/cmock/src
