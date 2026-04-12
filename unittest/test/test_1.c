eef12a3209e5c185f523a5a9f9daef861ff68b91
Hayden <haydendunnicliffe@gmail.com>
2026-04-12 12:53:13 +0100

    Make test pass

--- unittest/test/test_1.c
+++ unittest/test/test_1.c
@@ -26,9 +26,9 @@ void test_encoder_init_sets_states(void)
   encoder.pin_b = 2;
   encoder.pin_sw = 3;
 
-  hal_gpio_read_ExpectAndReturn(encoder.pin_a, 1);  // Return 1 for pin_a
-  hal_gpio_read_ExpectAndReturn(encoder.pin_b, 0);  // Return 1 for pin_b
-  hal_gpio_read_ExpectAndReturn(encoder.pin_sw, 1); // Return 1 for pin_sw
+  hal_gpio_read_ExpectAndReturn(encoder.pin_a, 1);
+  hal_gpio_read_ExpectAndReturn(encoder.pin_b, 0);
+  hal_gpio_read_ExpectAndReturn(encoder.pin_sw, 1);
 
   hal_gpio_callback_Expect(encoder.pin_a, _encoder_gpio_callback, &encoder);
   hal_gpio_callback_Expect(encoder.pin_b, _encoder_gpio_callback, &encoder);
@@ -39,6 +39,6 @@ void test_encoder_init_sets_states(void)
 
   // All pins current state should be set to 1/high
   TEST_ASSERT_EQUAL(1, encoder.pin_a_state);
-  TEST_ASSERT_EQUAL(1, encoder.pin_b_state);
-  TEST_ASSERT_EQUAL(0, encoder.pin_sw_state);
+  TEST_ASSERT_EQUAL(0, encoder.pin_b_state);
+  TEST_ASSERT_EQUAL(1, encoder.pin_sw_state);
 }
