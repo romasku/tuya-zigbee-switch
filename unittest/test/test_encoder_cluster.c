d8dde23b9aa5687efa4d2f03341b256ccf9a4c38
Hayden Dunnicliffe <hayden.dunnicliffe@booking.com>
2026-04-17 09:12:29 +0100

    Setup framework for encoder cluster tests

---
+++ unittest/test/test_encoder_cluster.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+
+void setUp(void)
+{
+  // Put a space between tests for readability
+  printf("\r\n");
+}
+
+void tearDown(void)
+{
+}
+
+void test_example(void)
+{
+  TEST_ASSERT_EQUAL(1, 2);
+}
