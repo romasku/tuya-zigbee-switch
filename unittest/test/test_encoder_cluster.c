580fee4425b3e50ff35cb08acf4d353a75734001
Hayden Dunnicliffe <hayden.dunnicliffe@booking.com>
2026-04-17 13:23:28 +0100

    get both encoder cluster tests working at same time

--- unittest/test/test_encoder_cluster.c
+++ unittest/test/test_encoder_cluster.c
@@ -3,16 +3,6 @@
 #include "zigbee/consts.h"
 #include "Mockzigbee.h"
 
-void setUp(void)
-{
-  // Put a space between tests for readability
-  printf("\r\n");
-}
-
-void tearDown(void)
-{
-}
-
 int send_cmd_call_count = 0;
 hal_zigbee_cmd captured_commands[10];
 hal_zigbee_status_t captured_send_cmd_to_bindings(const hal_zigbee_cmd *cmd, int cmock_num_calls)
@@ -22,41 +12,53 @@ hal_zigbee_status_t captured_send_cmd_to_bindings(const hal_zigbee_cmd *cmd, int
   return HAL_ZIGBEE_OK;
 }
 
-// void test_encoder_is_clicked(void)
-// {
-//   // Toggle On Off Zigbee commmand should be sent
+void setUp(void)
+{
+  // Put a space between tests for readability
+  printf("\r\n");
 
-//   hal_zigbee_endpoint mock_endpoint = {};
-//   mock_endpoint.endpoint = 1;
-//   mock_endpoint.cluster_count = 0;
-//   hal_zigbee_cluster endpoint_clusters[10];
-//   mock_endpoint.clusters = endpoint_clusters;
+  send_cmd_call_count = 0;
+}
 
-//   encoder_t mock_encoder = {};
+void tearDown(void)
+{
+}
 
-//   zigbee_encoder_cluster encoder_cluster = {};
-//   encoder_cluster.encoder = &mock_encoder;
+void test_encoder_is_clicked(void)
+{
+  // Toggle On Off Zigbee commmand should be sent
 
-//   encoder_cluster_add_to_endpoint(&encoder_cluster, &mock_endpoint);
+  hal_zigbee_endpoint mock_endpoint = {};
+  mock_endpoint.endpoint = 1;
+  mock_endpoint.cluster_count = 0;
+  hal_zigbee_cluster endpoint_clusters[10];
+  mock_endpoint.clusters = endpoint_clusters;
 
-//   hal_zigbee_get_network_status_ExpectAndReturn(HAL_ZIGBEE_NETWORK_JOINED);
+  encoder_t mock_encoder = {};
 
-//   hal_zigbee_send_cmd_to_bindings_Stub(captured_send_cmd_to_bindings);
+  zigbee_encoder_cluster encoder_cluster = {};
+  encoder_cluster.encoder = &mock_encoder;
 
-//   mock_encoder.on_press(mock_encoder.callback_param);
+  encoder_cluster_add_to_endpoint(&encoder_cluster, &mock_endpoint);
 
-//   // Check one command was sent 
-//   TEST_ASSERT_EQUAL(1, send_cmd_call_count);
+  hal_zigbee_get_network_status_ExpectAndReturn(HAL_ZIGBEE_NETWORK_JOINED);
 
-//   // Check command was toggle on/off
-//   TEST_ASSERT_EQUAL(mock_endpoint.endpoint, captured_commands[0]->endpoint);  
-//   TEST_ASSERT_EQUAL(ZCL_HA_PROFILE, captured_commands[0]->profile_id);  
-//   TEST_ASSERT_EQUAL(ZCL_CLUSTER_ON_OFF, captured_commands[0]->cluster_id);  
-//   TEST_ASSERT_EQUAL(ZCL_CMD_ONOFF_TOGGLE, captured_commands[0]->command_id);
-//   TEST_ASSERT_EQUAL(HAL_ZIGBEE_DIR_CLIENT_TO_SERVER, captured_commands[0]->direction);
-//   TEST_ASSERT_NULL(captured_commands[0]->payload);
-//   TEST_ASSERT_EQUAL(0, captured_commands[0]->payload_len);
-// }
+  hal_zigbee_send_cmd_to_bindings_Stub(captured_send_cmd_to_bindings);
+
+  mock_encoder.on_press(mock_encoder.callback_param);
+
+  // Check one command was sent 
+  TEST_ASSERT_EQUAL_MESSAGE(1, send_cmd_call_count, "Unexpected number of commands sent");
+
+  // Check command was toggle on/off
+  TEST_ASSERT_EQUAL(mock_endpoint.endpoint, captured_commands[0].endpoint);  
+  TEST_ASSERT_EQUAL(ZCL_HA_PROFILE, captured_commands[0].profile_id);  
+  TEST_ASSERT_EQUAL(ZCL_CLUSTER_ON_OFF, captured_commands[0].cluster_id);  
+  TEST_ASSERT_EQUAL(ZCL_CMD_ONOFF_TOGGLE, captured_commands[0].command_id);
+  TEST_ASSERT_EQUAL(HAL_ZIGBEE_DIR_CLIENT_TO_SERVER, captured_commands[0].direction);
+  TEST_ASSERT_NULL(captured_commands[0].payload);
+  TEST_ASSERT_EQUAL(0, captured_commands[0].payload_len);
+}
 
 void test_encoder_is_rotated_cw(void)
 {
@@ -82,7 +84,7 @@ void test_encoder_is_rotated_cw(void)
   mock_encoder.on_rotate_cw(mock_encoder.callback_param);
 
   // Check one command was sent 
-  TEST_ASSERT_EQUAL(1, send_cmd_call_count);
+  TEST_ASSERT_EQUAL_MESSAGE(1, send_cmd_call_count, "Unexpected number of commands sent");
 
   // Check command was step brightnes up
   TEST_ASSERT_EQUAL(mock_endpoint.endpoint, captured_commands[0].endpoint);  
--- unittest/test/test_runners/test_encoder_cluster_runner.c
+++ unittest/test/test_runners/test_encoder_cluster_runner.c
@@ -10,6 +10,7 @@
 /*=======External Functions This Runner Calls=====*/
 extern void setUp(void);
 extern void tearDown(void);
+extern void test_encoder_is_clicked(void);
 extern void test_encoder_is_rotated_cw(void);
 
 
@@ -75,7 +76,8 @@ static void run_test(UnityTestFunction func, const char* name, UNITY_LINE_TYPE l
 int main(void)
 {
   UnityBegin("test/test_encoder_cluster.c");
-  run_test(test_encoder_is_rotated_cw, "test_encoder_is_rotated_cw", 61);
+  run_test(test_encoder_is_clicked, "test_encoder_is_clicked", 27);
+  run_test(test_encoder_is_rotated_cw, "test_encoder_is_rotated_cw", 63);
 
   CMock_Guts_MemFreeFinal();
   return UNITY_END();
