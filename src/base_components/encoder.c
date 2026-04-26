1b9207d4160f2e480c6cc903ac01fdc5e3fa31c1
Hayden <haydendunnicliffe@gmail.com>
2026-04-26 13:43:06 +0100

    Implement better rotary encoder reading logic, which handles noise without dead time.

--- src/base_components/encoder.c
+++ src/base_components/encoder.c
@@ -5,6 +5,7 @@
 #include <stddef.h>
 
 void _encoder_gpio_callback(hal_gpio_pin_t pin, void *arg);
+void _rotate_callback(hal_gpio_pin_t pin, void *arg);
 void _pinAChanged(uint8_t new_state, encoder_t *encoder);
 void _pinBChanged(uint8_t new_state, encoder_t *encoder);
 void _pinSWChanged(uint8_t new_state, encoder_t *encoder);
@@ -20,11 +21,74 @@ void encoder_init(encoder_t *encoder)
   encoder->pin_sw_state = hal_gpio_read(encoder->pin_sw);
   encoder->pin_sw_last_change = hal_millis();
 
-  hal_gpio_callback(encoder->pin_a, _encoder_gpio_callback, encoder);
-  hal_gpio_callback(encoder->pin_b, _encoder_gpio_callback, encoder);
+  hal_gpio_callback(encoder->pin_a, _rotate_callback, encoder);
+  hal_gpio_callback(encoder->pin_b, _rotate_callback, encoder);
   hal_gpio_callback(encoder->pin_sw, _encoder_gpio_callback, encoder);
 
   encoder->rotate_since_pressed = false;
+}
+
+// Based on https://youtube.com/watch?v=fgOfSHTYeio
+// NOTE: Using a half step encoder (detent on high and low), so step size (encval) is reduced from 4 to 2 per step
+void _rotate_callback(hal_gpio_pin_t pin, void *arg) {
+  encoder_t *encoder = (encoder_t *)arg;
+
+  static uint8_t old_AB = 3;
+  static int8_t encval = 0;
+  static const int8_t enc_states[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
+
+  old_AB <<= 2;
+
+  if(hal_gpio_read(encoder->pin_a)) old_AB |= 0x02;
+  if(hal_gpio_read(encoder->pin_b)) old_AB |= 0x01;
+
+  encval += enc_states[(old_AB & 0x0f)];
+
+  if(encval > 1) {
+    encval = 0;
+    encoder->rotate_since_pressed = true;
+
+    if (encoder->pin_sw_state == 0)
+    {
+      printf("Rotating CW while Pressed\r\n");
+      if (encoder->on_rotate_cw_while_pressed != NULL)
+      {
+        encoder->on_rotate_cw_while_pressed(encoder->callback_param);
+      }
+    }
+    else
+    {
+      printf("Rotating CW\r\n");
+
+      if (encoder->on_rotate_cw != NULL)
+      {
+        encoder->on_rotate_cw(encoder->callback_param);
+      }
+    }
+
+  } 
+  else if (encval < -1) {
+    encval = 0;
+    encoder->rotate_since_pressed = true;
+
+    if (encoder->pin_sw_state == 0)
+    {
+      printf("Rotating CCW while Pressed\r\n");
+      if (encoder->on_rotate_ccw_while_pressed != NULL)
+      {
+        encoder->on_rotate_ccw_while_pressed(encoder->callback_param);
+      }
+    }
+    else
+    {
+      printf("Rotating CCW\r\n");
+      if (encoder->on_rotate_ccw != NULL)
+      {
+        encoder->on_rotate_ccw(encoder->callback_param);
+      }
+    }
+
+  }
 }
 
 void _encoder_gpio_callback(hal_gpio_pin_t pin, void *arg)
